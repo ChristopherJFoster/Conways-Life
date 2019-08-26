5a778664a867688d5835300c5ddec059f60a994c
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-08-25 20:15:24 -0400

    Working on scaling buttons according to breakpoints.

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import cc from 'classcat';
 import makeStyles from '@material-ui/styles/makeStyles';
-// import useTheme from '@material-ui/styles/useTheme';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
@@ -32,8 +32,22 @@ const useStyles = makeStyles(theme => ({
   bottomRow: {
     padding: '10px 0',
     display: 'grid',
-    gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
     justifyContent: 'space-between',
+    [theme.breakpoints.down('xl')]: {
+      gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.down('md')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.down('sm')]: {
+      gridTemplateColumns: 'repeat(4, minmax(35px, 35px)) minmax(109px, 109px)',
+    },
+    [theme.breakpoints.down('xs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.only('xxs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
   },
   playPauseButton: {
     background: theme.palette.dead.a,
@@ -45,6 +59,29 @@ const useStyles = makeStyles(theme => ({
       backgroundColor: theme.palette.dead.c,
       fill: theme.palette.alive.i,
     },
+    [theme.breakpoints.down('xl')]: {
+      minWidth: '0',
+      minHeight: '0',
+      width: '60px',
+      height: '60px',
+    },
+    [theme.breakpoints.down('md')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '40px',
+      height: '40px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.only('xxs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+  },
+  playPauseSVG: {
+    width: '100%',
+    height: 'auto',
   },
   randomButton: {
     background: theme.palette.dead.a,
@@ -54,14 +91,35 @@ const useStyles = makeStyles(theme => ({
       border: `3px solid ${theme.palette.alive.i}`,
       background: theme.palette.dead.c,
     },
+    [theme.breakpoints.down('xl')]: {
+      minWidth: '0',
+      minHeight: '0',
+      width: '60px',
+      height: '60px',
+    },
+    [theme.breakpoints.down('md')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '40px',
+      height: '40px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.only('xxs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+  },
+  randomIconSVG: {
+    position: 'absolute',
+    width: '75%',
+    padding: '0',
   },
   randomIconBack: {
-    position: 'absolute',
     fill: theme.palette.dead.i,
   },
   randomIconFore: {
-    padding: '13px',
-    position: 'absolute',
     fill: theme.palette.alive.a,
     '&:hover': {
       fill: theme.palette.alive.i,
@@ -75,9 +133,28 @@ const useStyles = makeStyles(theme => ({
       border: `3px solid ${theme.palette.alive.i}`,
       background: theme.palette.dead.c,
     },
+    [theme.breakpoints.down('xl')]: {
+      minWidth: '0',
+      minHeight: '0',
+      width: '60px',
+      height: '60px',
+    },
+    [theme.breakpoints.down('md')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '40px',
+      height: '40px',
+    },
+    [theme.breakpoints.down('xs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
+    [theme.breakpoints.only('xxs')]: {
+      // gridTemplateColumns: 'repeat(4, minmax(45px, 45px)) minmax(140px, 140px)',
+    },
   },
-  clearIconBack: {
-    fill: theme.palette.dead.i,
+  clearButtonSVG: {
+    width: '100%',
   },
 }));
 
@@ -95,7 +172,6 @@ export default function Controls({
   setIsRunning,
 }) {
   const classes = useStyles();
-  // const theme = useTheme();
 
   return (
     <div className={classes.container}>
@@ -108,34 +184,35 @@ export default function Controls({
       </div>
       <div className={classes.bottomRow}>
         <Button className={classes.playPauseButton} onClick={e => playPause(e)}>
-          <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40'>
+          <svg
+            className={classes.playPauseSVG}
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 40 40'
+          >
             <path d='M17.78 18.89h-2.22v-2.22h-4.45v-2.23H6.67v-2.22H2.22V10H0v20h2.22v-2.22h4.45v-2.22h4.44v-2.23h4.45v-2.22H20v-2.22h-2.22zM28.89 10h4.44v20h-4.44zM35.56 10H40v20h-4.44z' />
           </svg>
         </Button>
         <Button className={classes.playPauseButton} onClick={e => step(e)}>
           <svg
-            className={classes.stepIcon}
+            className={classes.playPauseSVG}
             xmlns='http://www.w3.org/2000/svg'
-            width='40'
-            height='40'
+            viewBox='0 0 40 40'
           >
             <path d='M27.78 10v8.89h-4.45v-2.22h-4.44v-2.23h-4.45v-2.22H10V10H7.78v20H10v-2.22h4.44v-2.22h4.45v-2.23h4.44v-2.22h4.45V30h4.44V10h-4.44z' />
           </svg>
         </Button>
         <Button className={classes.randomButton} onClick={e => random(e)}>
           <svg
-            className={classes.randomIconBack}
+            className={cc([classes.randomIconSVG, classes.randomIconBack])}
             xmlns='http://www.w3.org/2000/svg'
-            width='40'
-            height='40'
+            viewBox='0 0 40 40'
           >
             <path d='M0 0h40v40H0z' />
           </svg>
           <svg
-            className={classes.randomIconFore}
+            className={cc([classes.randomIconSVG, classes.randomIconFore])}
             xmlns='http://www.w3.org/2000/svg'
-            width='40'
-            height='40'
+            viewBox='0 0 40 40'
           >
             <path d='M35 0h5v5h-5zM35 10h5v5h-5zM30 5h-5v10h-5v10h5v-5h10v-5h-5v-5h5V5h-5z' />
             <path d='M20 5h5V0H10v5h5v10h5V5zM15 20h-5v-5H0v5h5v5h10v-5zM0 25h5v5H0zM25 25h10v5H25zM10 30H5v5H0v5h15v-5h-5v-5zM35 40h5V30h-5v10zM5 5h5v5H5zM0 0h5v5H0zM20 25h-5v10h5v5h5V30h-5v-5z' />
@@ -143,10 +220,9 @@ export default function Controls({
         </Button>
         <Button className={classes.clearButton} onClick={e => clear(e)}>
           <svg
-            className={classes.clearButtonIcon}
+            className={classes.clearButtonSVG}
             xmlns='http://www.w3.org/2000/svg'
-            width='40'
-            height='40'
+            viewBox='0 0 40 40'
           >
             <path d='M0 0h40v40H0z' />
           </svg>
